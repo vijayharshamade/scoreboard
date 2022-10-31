0eba970a902a8c0a351015b826695b3593c4df38
vijayharshamade <vharsha736.vh@gmail.com>
2022-10-31 10:14:13 +0000

    useEffect part 1

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,7 @@
+// // import {useState, useEffect} from 'react'
+
+// const Welcome = () => <h1>Welcome</h1>
+
+// export default Welcome
+
+// this is not necessary just an example to explain other cod ein the class
